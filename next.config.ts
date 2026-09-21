6209da96e84506a759af53dea5db83315e6d64bf
Rodrigo Luna Barcelos <rodrigolunabarcelos@gmail.com>
2026-09-21 00:19:23 -0400

    Redirect /map to the home page

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [{ source: "/map", destination: "/", permanent: true }];
+  },
   /* config options here */
 };
 
